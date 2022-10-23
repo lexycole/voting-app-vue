19f586705faa1c2935846dfdb289e560580f9377
lexycole <akinbolawa90@gmail.com>
2022-10-23 08:24:40 +0100

    add files

---
+++ js/seed.js
@@ -0,0 +1,51 @@
+window.Seed = (function () {
+    const submissions = [
+      {
+        id: 1,
+        title: 'UEFA Champions League',
+        description: 'Maupay on target as Brighton sink lacklustre Leads',
+        url: '#',
+        votes: 11,
+        avatar: './img/Avatars/veronika.jpg',
+        submissionImage: './img/avtar-2.jpg',
+      },
+      {
+        id: 2,
+        title: 'Fantasy Congress League',
+        description: 'Fernandes: Individual awards good but i want trophies with Man Utd.',
+        url: '#',
+        votes: 9,
+        avatar: './img/Avatars/molly.png',    
+        submissionImage: './img/avtar-4.jpg',
+      },
+      {
+        id: 3,
+        title: 'Europa Champions League',
+        description: 'Copa del Rey: Sadiq at the double as Almeria sink Alaves.',
+        url: '#',
+        votes: 7,
+        avatar: './img/Avatars/kristy.png',
+        submissionImage: './img/avtar-6.jpg',
+      },
+      {
+        id: 4,
+        title: 'League One Champions',
+        description: 'Ajayi on taget as West Brom beat Wolves 3 - 2.',
+        url: '#',
+        votes: 4,
+        avatar: './img/Avatars/daniel.jpg',
+        submissionImage: './img/avtar-3.jpg',
+      },
+      // {
+      //   id: 5,
+      //   title: 'Haught or Naught',
+      //   description: 'High-minded or absent-minded? You decide.',
+      //   url: '#',
+      //   votes: 19,
+      //   avatar: './img/submissions/avtar-8.jpg',
+      //   submissionImage: './img/avtar-3.jpg',
+      // }
+    ];
+  
+    return { submissions: submissions };
+}());
